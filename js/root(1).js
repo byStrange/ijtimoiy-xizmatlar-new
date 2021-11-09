d1a6a5bbf26c34be495f5a9fea87d330c807f6dd
Strange Stephen <80538114+Strange-bs@users.noreply.github.com>
2021-11-09 15:02:58 +0500

    update

--- js/root(1).js
+++ js/root(1).js
@@ -44,3 +44,55 @@ closer()
 // 		closer()
 // 	}
 // }
+let container = document.createElement('div');
+function createOpen() {
+	container.className = "modal-cont";
+	let html = `
+	<div class="modal-bx box-mini" style="min-height:0;">
+            <form class="form">
+                <h4><span class="item-type"></span>Ariza tekshirish</h4>
+                <div class="form-group">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="pname">Ariza turi</label>
+								<select class="form-control" required>
+									<option disabled>Tanlang</option>
+									<option>Sihatgoh uchun ariza</option>
+									<option>Tibbiy buyum uchun ariza</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="series">Pasport seriyasi</label>
+								<input type="submit" class="form-control btn btn-primary" id="submit" value="Ariza berish" required="">
+							</div>
+						</div>
+					</div>
+				</div>
+                <span class="close-modal" tabindex="1" onclick="closee()">&times</span>
+            </form>
+        </div>`
+		container.innerHTML = html;
+		document.body.appendChild(container);
+		anime({
+			targets:['.modal-bx.box-mini', '.modal-box.box-mini .form-group'],
+			translateX:  [function () {return anime.random(-1200,100)}, 0],
+			translateY : [function () {return anime.random(-1300,100)},0],
+			opacity : [0,1],
+			delay: anime.stagger(150),
+			easing: 'easeOutExpo'
+		})
+		document.querySelector('.modal-bx.box-mini').parentElement.style = 'background:rgba(0,0,0,.5);z-index:5'
+}
+
+function closee() {
+	anime({
+		targets: ['.modal-bx.box-mini', '.modal-bx.box-mini.form-group'],
+		translateX: [0,function () {return anime.random(-550,50)}],
+		translateY: [0,function () {return anime.random(-550,50)}],
+		opacity: 0
+	})
+		document.querySelector('.modal-bx.box-mini').parentElement.style = 'background:transparent;z-index:-5'
+}
